d41f7b30611fd2fe2dbe410511f32e9f5e08cbbf
hoangbkcntt98 <bugger485@gmail.com>
2021-10-27 23:07:45 +0700

    fitler ok

--- src/components/header/autocomplete/autosuggest/AutoSuggest.jsx
+++ src/components/header/autocomplete/autosuggest/AutoSuggest.jsx
@@ -85,7 +85,6 @@ const languages = [
     }
 ];
 
-// https://developer.mozilla.org/en/docs/Web/JavaScript/Guide/Regular_Expressions#Using_Special_Characters
 function escapeRegexCharacters(str) {
     return str.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
 }
