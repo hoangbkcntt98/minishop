b41b101c4b61d9585af1c1434d9a9119a2e1a525
hoangbkcntt98 <bugger485@gmail.com>
2021-10-28 14:59:24 +0700

    fix-bug-search

--- src/components/header/autocomplete/autosuggest/AutoSuggest.jsx
+++ src/components/header/autocomplete/autosuggest/AutoSuggest.jsx
@@ -1,10 +1,10 @@
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import Autosuggest from "react-autosuggest";
 import SearchIcon from '@mui/icons-material/Search'
 import productData from '../../../../assets/fake-data/products'
 import category from '../../../../assets/fake-data/category'
 import { SuggestionItem } from "./SuggestionItem";
-import { useSelector,useDispatch } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { filterSelectRedux } from "../../../../redux/product/ProductSlice";
 
 import { useHistory } from "react-router";
@@ -15,44 +15,42 @@ const AutoSuggest = () => {
     const filterState = useSelector((state) => state.product.filter)
     const dispatch = useDispatch()
     const history = useHistory();
+    const [data, setData] = useState([])
     useEffect(() => {
-        console.log(filterState)
+        let temp = category.map(cate => {
+            let productCate = products.filter(p => p.categorySlug == cate.categorySlug)
+            productCate = productCate.map(item => {
+                return {
+                    name: item.title,
+                    price: item.price,
+                    img: item.image01
+                }
+
+
+            })
+            return {
+                title: cate.display,
+                products: productCate
+            }
+        })
+        setData(temp)
+        // console.log(filterState)
     }, [filterState])
-    // useEffect(() => {
-    //     console.log(value)
-       
-    // }, [value])
-    const handleSubmit = () =>{
+    const handleSubmit = () => {
         // alert(value)
-        dispatch(filterSelectRedux({type:"NAME",checked:true,item:value}))
+        dispatch(filterSelectRedux({ type: "NAME", checked: true, item: value }))
         history.push('/catalog')
     }
-    // console.log(products)
-    var data = []
-   
-    data = category.map(cate => {
-        let productCate = products.filter(p => p.categorySlug == cate.categorySlug)
-        productCate = productCate.map(item => {
-            return {
-                name: item.title,
-                price: item.price,
-                img:item.image01
-            }
-
-
-        })
-        return {
-            title: cate.display,
-            products: productCate
-        }
-    })
-    // console.log(data)
 
     function getSuggestions(value) {
-        let rs = data.filter(lang =>{
-            let products = lang.products.filter(item => item.name.toLowerCase().includes(value.toLowerCase()))
-            if(products.length>0) return lang
+        let rs = data.map(item => {
+            let products = item.products.filter(item => item.name.toLowerCase().includes(value.toLowerCase()))
+            if (products.length > 0)return {
+                title:item.title,
+                products:products
+            }
         })
+        rs = rs.filter(item => item!=undefined&&item.products!=undefined)
         console.log(rs)
         return rs;
     }
@@ -63,7 +61,7 @@ const AutoSuggest = () => {
 
     function renderSuggestion(suggestion) {
         return (
-            <div className= "react-autosuggest__result">
+            <div className="react-autosuggest__result">
                 <SuggestionItem thumbanilSrc={suggestion.img} name={suggestion.name} price={suggestion.price}></SuggestionItem>
             </div>
             // <span>{suggestion.name}</span>
@@ -86,7 +84,7 @@ const AutoSuggest = () => {
 
     const onSuggestionsFetchRequested = ({ value }) => {
         setSuggestion(getSuggestions(value))
-        
+
     };
 
     const onSuggestionsClearRequested = () => {
@@ -110,7 +108,7 @@ const AutoSuggest = () => {
                 renderSectionTitle={renderSectionTitle}
                 getSectionSuggestions={getSectionSuggestions}
                 inputProps={inputProps} />
-            <button className='react-autosuggest__button' onClick = {handleSubmit}><SearchIcon></SearchIcon></ button>
+            <button className='react-autosuggest__button' onClick={handleSubmit}><SearchIcon></SearchIcon></ button>
         </React.Fragment>
     );
     // }
