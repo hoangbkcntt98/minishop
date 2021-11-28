62a94e6a9fa52fa7b147fc839acc51b8ce03dc90
hoangbkcntt98 <bugger485@gmail.com>
2021-11-28 09:39:02 +0700

    filter ok

--- src/services/api.js
+++ src/services/api.js
@@ -2,5 +2,5 @@ import axios from 'axios';
 
 export default axios.create({
   // baseURL: `https://protected-river-26160.herokuapp.com/api/`
-  baseURL: `http://localhost:5000/`
+  baseURL: process.env.NODE_ENV == "development"?`http://localhost:5000/`:`https://minishop-node-server.herokuapp.com/`
 });
