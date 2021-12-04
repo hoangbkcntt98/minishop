592a0d803c73b579dec39cb9b86e8e33ab74d503
hoangbkcntt98 <bugger485@gmail.com>
2021-12-04 19:14:45 +0700

    slider-complete

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -14,98 +14,98 @@ import { setCategories,setProducts, product, setLinks, setLoading, setTotalPage,
 const Routes = () => {
     const dispatch = useDispatch()
     
-    useEffect(()=>{
-        const formatIndex = (index) =>{
-            if(index.length==0) return index
-            let other = index.find(item => item.display.toUpperCase().includes("KHÁC"))
-            if(other ){
-                console.log('other',other)
-                index = index.filter(item => !item.display .toUpperCase().includes("KHÁC"))
-                index.push(other)
-                console.log('child formated',index)
-            }
+    // useEffect(()=>{
+    //     const formatIndex = (index) =>{
+    //         if(index.length==0) return index
+    //         let other = index.find(item => item.display.toUpperCase().includes("KHÁC"))
+    //         if(other ){
+    //             console.log('other',other)
+    //             index = index.filter(item => !item.display .toUpperCase().includes("KHÁC"))
+    //             index.push(other)
+    //             console.log('child formated',index)
+    //         }
           
-            return index
-        }
-        productServices.getProductAttributes({ type: "CATE" }).then(res => {
-            let initIndex = [
-                {
-                    root: "Áo",
-                    categorySlug: "a",
-                    type: 1,
-                    open: false,
-                    child: []
-                },
-                {
-                    root: "Quần",
-                    categorySlug: "q",
-                    open: false,
-                    child: [],
-                    type: 2
-                },
-                {
-                    root: "Váy",
-                    categorySlug: "a",
-                    open: false,
-                    child: [],
-                    type: 3
-                },
+    //         return index
+    //     }
+    //     productServices.getProductAttributes({ type: "CATE" }).then(res => {
+    //         let initIndex = [
+    //             {
+    //                 root: "Áo",
+    //                 categorySlug: "a",
+    //                 type: 1,
+    //                 open: false,
+    //                 child: []
+    //             },
+    //             {
+    //                 root: "Quần",
+    //                 categorySlug: "q",
+    //                 open: false,
+    //                 child: [],
+    //                 type: 2
+    //             },
+    //             {
+    //                 root: "Váy",
+    //                 categorySlug: "a",
+    //                 open: false,
+    //                 child: [],
+    //                 type: 3
+    //             },
                 
-                {
-                    root: "Set",
-                    categorySlug: "S",
-                    // type: 4,
-                    open: false,
-                    child: []
-                },
-                {
-                    root: "Khác",
-                    categorySlug: "OTHER",
-                    open: false,
-                    child: [],
+    //             {
+    //                 root: "Set",
+    //                 categorySlug: "S",
+    //                 // type: 4,
+    //                 open: false,
+    //                 child: []
+    //             },
+    //             {
+    //                 root: "Khác",
+    //                 categorySlug: "OTHER",
+    //                 open: false,
+    //                 child: [],
                     
-                },
-            ]
-            initIndex = initIndex.map(item => {
-                let child = res.filter(c => c.sub_type == item.type).map(val => {
-                    return {
-                        display: val.display,
-                        categorySlug: val.code
-                    }
-                })
-                item.child = formatIndex(child)
-                console.log('child',child)
-                return item
-            })
-            // setIndexs(initIndex)
-            dispatch(setCategories(initIndex))
-        })
-        productServices.getProductAttributes({type:"COLOR"}).then(res =>{
-            // console.log(res)
-            let temp = res.map(r => {
-                return {
-                    display:r.display,
-                    color:r.code
-                }
-            })
-            dispatch(setColorsRedux(temp))
-        })
-        productServices.getProductAttributes({type:"SIZE"}).then(res => {
-            let temp = res.map(r => {
-                return {
-                    display:r.display,
-                    size:r.code
-                }
-            })
-            dispatch(setSizesRedux(temp))
-        })
-        productServices.getProducts().then((res) => {
+    //             },
+    //         ]
+    //         initIndex = initIndex.map(item => {
+    //             let child = res.filter(c => c.sub_type == item.type).map(val => {
+    //                 return {
+    //                     display: val.display,
+    //                     categorySlug: val.code
+    //                 }
+    //             })
+    //             item.child = formatIndex(child)
+    //             console.log('child',child)
+    //             return item
+    //         })
+    //         // setIndexs(initIndex)
+    //         dispatch(setCategories(initIndex))
+    //     })
+    //     productServices.getProductAttributes({type:"COLOR"}).then(res =>{
+    //         // console.log(res)
+    //         let temp = res.map(r => {
+    //             return {
+    //                 display:r.display,
+    //                 color:r.code
+    //             }
+    //         })
+    //         dispatch(setColorsRedux(temp))
+    //     })
+    //     productServices.getProductAttributes({type:"SIZE"}).then(res => {
+    //         let temp = res.map(r => {
+    //             return {
+    //                 display:r.display,
+    //                 size:r.code
+    //             }
+    //         })
+    //         dispatch(setSizesRedux(temp))
+    //     })
+    //     productServices.getProducts().then((res) => {
 
-            dispatch(setProducts(res.products))
-            dispatch(setTotalPage(parseInt(res.total_pages/12)+1))
-            // dispatch(setLoading(false))
-        })
-    },[])
+    //         dispatch(setProducts(res.products))
+    //         dispatch(setTotalPage(parseInt(res.total_pages/12)+1))
+    //         // dispatch(setLoading(false))
+    //     })
+    // },[])
     return (
         <Switch>
             <Route path='/' exact component={Home}/>
