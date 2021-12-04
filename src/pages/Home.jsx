0d9e224ddcb19adbd3c5be84fca0ad845abc622e
hoangbkcntt98 <bugger485@gmail.com>
2021-12-04 19:55:21 +0700

    fix-bug-dep-next

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,117 +14,14 @@ import heroSliderData from '../assets/fake-data/hero-slider'
 import banner from '../assets/images/banner.png'
 import ad1 from '../assets/images/banner1.jpg'
 import Slider from '../components/slider/Slider'
-import {useEffect,useState} from 'react'
-import { useSelector } from 'react-redux'
-import { useDispatch } from 'react-redux'
-import productServices from '../services/productServices'
-import { setCategories,setProducts, product, setLinks, setLoading, setTotalPage, setProduct, setColorsRedux, setSizesRedux } from '../redux/product/ProductSlice'
-const Home = () => {
-    const dispatch = useDispatch()
-    const productRedux = useSelector((state) => state.product.products)
-    useEffect(()=>{
-        const formatIndex = (index) =>{
-            if(index.length==0) return index
-            let other = index.find(item => item.display.toUpperCase().includes("KHÁC"))
-            if(other ){
-                console.log('other',other)
-                index = index.filter(item => !item.display .toUpperCase().includes("KHÁC"))
-                index.push(other)
-                console.log('child formated',index)
-            }
-          
-            return index
-        }
-        productServices.getProductAttributes({ type: "CATE" }).then(res => {
-            let initIndex = [
-                {
-                    root: "Áo",
-                    categorySlug: "a",
-                    type: 1,
-                    open: false,
-                    child: []
-                },
-                {
-                    root: "Quần",
-                    categorySlug: "q",
-                    open: false,
-                    child: [],
-                    type: 2
-                },
-                {
-                    root: "Váy",
-                    categorySlug: "a",
-                    open: false,
-                    child: [],
-                    type: 3
-                },
-                
-                {
-                    root: "Set",
-                    categorySlug: "S",
-                    // type: 4,
-                    open: false,
-                    child: []
-                },
-                {
-                    root: "Khác",
-                    categorySlug: "OTHER",
-                    open: false,
-                    child: [],
-                    
-                },
-            ]
-            initIndex = initIndex.map(item => {
-                let child = res.filter(c => c.sub_type == item.type).map(val => {
-                    return {
-                        display: val.display,
-                        categorySlug: val.code
-                    }
-                })
-                item.child = formatIndex(child)
-                console.log('child',child)
-                return item
-            })
-            // setIndexs(initIndex)
-            dispatch(setCategories(initIndex))
-        })
-        productServices.getProductAttributes({type:"COLOR"}).then(res =>{
-            // console.log(res)
-            let temp = res.map(r => {
-                return {
-                    display:r.display,
-                    color:r.code
-                }
-            })
-            dispatch(setColorsRedux(temp))
-        })
-        productServices.getProductAttributes({type:"SIZE"}).then(res => {
-            let temp = res.map(r => {
-                return {
-                    display:r.display,
-                    size:r.code
-                }
-            })
-            dispatch(setSizesRedux(temp))
-        })
-        productServices.getProducts().then((res) => {
 
-            dispatch(setProducts(res.products))
-            dispatch(setTotalPage(parseInt(res.total_pages/12)+1))
-            // dispatch(setLoading(false))
-        })
-    },[])
+const Home = () => {
     return (
         <Helmet title="Trang chủ">
+                
             <Section>
                 <div className="banner">
                     <Slider />
-                    {/* <HeroSlider
-                        data={heroSliderData}
-                        control={true}
-                        auto={false}
-                        timeOut={5000}
-                    /> */}
                     <div className="banner__ads">
                         <div className="banner__ads__image"><img src={ad1} alt=""></img></div>
                         <div className="banner__ads__image"><img src={ad1} alt=""></img></div>
